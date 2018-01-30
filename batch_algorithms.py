9f7e2b5099cdb702b3b9010082ac17c791399e8f
Ahmed Touati <ahmed.touati90@gmail.com>
2018-01-30 17:16:25 -0500

    implem AB-Trace

--- batch_algorithms.py
+++ batch_algorithms.py
@@ -55,8 +55,8 @@ def batch_off_policy(value_function, target_policy, data, lambda_param, discount
     return errors
 
 
-def batch_gradient_off_policy(value_function, target_policy, data, lambda_param, discount_factor, type, num_episodes,
-                              obj_function, alpha):
+def batch_gradient_off_policy(value_function, target_policy, data, lambda_param, discount_factor, return_type, num_episodes,
+                              obj_function, alpha_omega_0, alpha_theta_0):
     nS = value_function.nS
     nA = value_function.nA
 
@@ -79,16 +79,23 @@ def batch_gradient_off_policy(value_function, target_policy, data, lambda_param,
                     episode['rewards'],
                     episode['behavior_probs'],
                     episode['target_probs'])):
+            iteration = i * len(episode['states']) + idx
+
+            if i % 20 == 0:
+                alpha_omega = alpha_omega_0 / np.sqrt(i + 1)
+                alpha_theta = alpha_theta_0 / np.sqrt(i + 1)
             old_omega = omega.copy()
             old_theta = theta.copy()
 
             phi = value_function.feature(state, action)
             q = np.dot(old_theta, phi)
 
-            if type == 'TB':
+            if return_type == 'TB':
                 kappa = target_prob
-            elif type == 'Retrace':
+            elif return_type == 'Retrace':
                 kappa = min([1, target_prob / behavior_prob])
+            else:
+                raise NotImplementedError()
 
             e *= discount_factor * lambda_param * kappa
             e += phi
@@ -104,10 +111,74 @@ def batch_gradient_off_policy(value_function, target_policy, data, lambda_param,
             td_target = reward + discount_factor * V
             delta = td_target - q
 
-            omega = old_omega + alpha * (delta * e - np.dot(old_omega, phi) * phi)
+            omega = old_omega + alpha_omega * (delta * e - np.dot(old_omega, phi) * phi)
 
-            theta = old_theta - alpha * np.dot(old_omega, e) * \
+            theta = old_theta - alpha_theta * np.dot(old_omega, e) * \
                                 (discount_factor * expected_phiprime - phi)
+        if i > num_episodes:
+            break
+    return errors
+
+
+def batch_AB_Trace(value_function, target_policy, behavior_policy, data, lambda_param, discount_factor, num_episodes,
+                   obj_function, alpha_0):
+    nS = value_function.nS
+    nA = value_function.nA
+
+    theta = np.zeros(value_function.param_shape)
+    omega = np.zeros(theta.shape)
+    errors = []
+
+    for i, episode in enumerate(data):
+        error = obj_function(theta)
+        errors.append(error)
+
+        if i % 10 == 0:
+            print('episode %d, error %f' % (i, error))
+        # initialising eligibility traces
+        e = np.zeros(shape=theta.shape)
+        for idx, (state, action, next_state, reward, behavior_prob, target_prob) in enumerate(
+                zip(episode['states'][:-1],
+                    episode['actions'],
+                    episode['states'][1:],
+                    episode['rewards'],
+                    episode['behavior_probs'],
+                    episode['target_probs'])):
+            iteration = i * len(episode['states']) + idx
+
+            if i % 20 == 0:
+                alpha_omega = alpha_0 / np.sqrt(i + 1)
+                alpha_theta = alpha_0 / (i + 1)
+            old_omega = omega.copy()
+            old_theta = theta.copy()
+
+            phi = value_function.feature(state, action)
+            q = np.dot(old_theta, phi)
+
+            kappa = min([1, target_prob / behavior_prob])
+
+            e *= discount_factor * lambda_param * kappa
+            e += phi
+
+            if idx == len(episode['states']) - 1:
+                expected_phiprime = 0
+                weighted_phiprime = 0
+            else:
+                expected_phiprime = np.sum(
+                    [target_policy[next_state, a] * value_function.feature(next_state, a)
+                     for a in np.arange(nA)], axis=0)
+                weighted_phiprime = np.sum(
+                    [min(1, target_policy[next_state, a]/behavior_policy[next_state, a]) * value_function.feature(next_state, a)
+                     for a in np.arange(nA)], axis=0)
+
+            V = np.dot(old_theta, expected_phiprime)
+            td_target = reward + discount_factor * V
+            delta = td_target - q
+
+            omega = old_omega + alpha_omega * (delta * e - np.dot(old_omega, phi) * phi)
+
+            theta = old_theta + alpha_theta * (delta * e - discount_factor * np.dot(old_omega, e) *
+                                            (expected_phiprime - weighted_phiprime))
         if i > num_episodes:
             break
     return errors
